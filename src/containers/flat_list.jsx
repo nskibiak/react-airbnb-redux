a0d6a72fbe364132c0ff1bdb235e703398afe6f4
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-03-29 07:59:37 +0900

    added static props to flat_list

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class FlatList extends Component {
+  static defaultProps = {
+    flats: [{
+      'name': 'Charm at the Steps of Montmartre',
+      'imageUrl': 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg',
+      'price': 164,
+      'priceCurrency': 'EUR'
+    }]
+  }
+
   render() {
     return (
       <div className="flat-list col-sm-7">
