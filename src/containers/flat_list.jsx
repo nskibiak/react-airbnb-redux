fbebaebde3b78796ba277f8cd5c19833e20c0e7b
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-03-29 07:56:20 +0900

    created container templates and flat_list scss

---
+++ src/containers/flat_list.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FlatList extends Component {
+  render() {
+    return (
+      <div className="flat-list col-sm-7">
+        TODO Flat List
+      </div>
+    );
+  }
+}
+
+export default FlatList;
