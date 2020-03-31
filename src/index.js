75d0fbab4cf079d09f20ae7988a3598a32dce9a1
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-03-31 15:48:23 +0900

    created index js in actions folder

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './stylesheets/index.scss';
-import App from './components/app';
-import store from './app/store';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
-// import flatsReducer from './reducers/flats_reducer';
-import * as serviceWorker from './serviceWorker';
+
+import './stylesheets/index.scss';
+
+// import store from './app/store';
+// import * as serviceWorker from './serviceWorker';
+import flatsReducer from './reducers/flats_reducer';
+
+import App from './components/app';
 
 const reducers = combineReducers({
-  // flats: flatsReducer
+  flats: flatsReducer
 });
 
 const root = document.getElementById('root');
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+  <App />
+  </Provider>,
+  root);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
