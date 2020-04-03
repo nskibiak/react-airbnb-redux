a29404f22fe780bf775e2bf29add68ea31c41693
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-03 13:28:53 +0900

    made google map container not component

--- src/containers/map.jsx
+++ src/containers/map.jsx
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
 class Map extends Component {
-
   render() {
-    const style = {
-      height: "100vh"
-    }
+    let marker = null;
+    let center = { lat: 48.856614, lng: 2.352222 };
 
     return (
-      <div className="col-sm-5" style={style}>
-        TODO Map
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          // bootstrapURLKeys={{ key: process.env.REACT_APP_MAP_API }}
+          defaultCenter={center}
+          defaultZoom={15}
+        >
+          {marker}
+        </GoogleMapReact>
       </div>
     );
   }
 }
 
 export default Map;
+
+// require('dotenv').config({ path: '../..' })
