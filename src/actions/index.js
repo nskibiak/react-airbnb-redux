73e1f67f98d06f8ce30d28b1a142cc4ff8278fd4
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-04-07 14:44:33 +0900

    added fetch function to set flats action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,14 @@ import flats from '../data/flats';
 
 export function setFlats() {
   // TODO: Api call! for now, simulate a DB
-
-  return {
-    type: 'SET_FLATS',
-    payload: flats
-  }
+  return fetch('https://raw.githubusercontent.com/lewagon/flatsboilerplate/master/flats.json')
+    .then(response => response.json())
+    .then((data) => {
+      return {
+        type: 'SET_FLATS',
+        payload: data
+      };
+    });
 }
 
 export function selectFlat(flat) {
